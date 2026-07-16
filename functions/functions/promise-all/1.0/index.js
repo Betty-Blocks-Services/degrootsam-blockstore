e36478d27f507055bd4062e717fade6959e2df06
Sam <39275315+degrootsam@users.noreply.github.com>
2026-07-16 15:34:29 +0200

    feat: added a Promise All function to concurrenty run multiple async requests per loop

--- functions/functions/promise-all/1.0/index.js
+++ functions/functions/promise-all/1.0/index.js
@@ -1,18 +1,20 @@
-const promiseAll = async ({ concurrency }, steps) => {
-  const promiseSets = [];
-  let set = [];
-  console.log(steps);
-  // for (const step of steps) {
-  //   for (let i = 0; i < concurrency; i++) {
-  //     set.push(step);
-  //   }
-  //   promiseSets.push(set);
-  //   set = [];
-  // }
-  //
-  // for (const promiseSet of promiseSets) {
-  //   await Promise.all(promiseSet);
-  // }
+import { normalizeArray } from "../../utils/array-utils";
+
+const promiseAll = async ({ concurrency, array }, steps) => {
+  const normalizedArray = normalizeArray(array);
+  if (!Array.isArray(normalizedArray)) {
+    throw new Error("Promise All: 'array' is required!");
+  }
+  const batchSize = concurrency > 0 ? concurrency : normalizedArray.length;
+
+  for (let i = 0; i < normalizedArray.length; i += batchSize) {
+    const batch = normalizedArray.slice(i, i + batchSize);
+    await Promise.all(
+      batch.map((value, offset) =>
+        steps({ iterator: value, index: i + offset }),
+      ),
+    );
+  }
 };
 
 export default promiseAll;
